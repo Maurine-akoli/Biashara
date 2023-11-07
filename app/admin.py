d89e12eb784c627b1cc7e6d87760cf4c35bab56f
Maurine-akoli <akolimaurine20@gmail.com>
2023-11-07 16:56:57 +0300

    initial commit

---
+++ app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from app.models import Member,Product
+
+
+# Register your models here.
+admin.site.register(Member)
+admin.site.register(Product)
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.firstnsme + " " + self.lastname
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    price = models.IntegerField(default=0)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
